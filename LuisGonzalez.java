c1b7fe7fa501d7fb2cf6f52af770814208140528
Luis Gonzalez <lg02393@gmail.com>
2016-10-12 21:23:59 +0200

    0607 5.

--- LuisGonzalez.java
+++ LuisGonzalez.java
@@ -1,4 +1,4 @@
 public class LuisGonzalez {
 	public static void main (String args[])
-	{ System.out.println("Luis Gonzalez de Haro\nEdad:23");
+	{ System.out.println("Luis Gonzalez de Haro\nEdad:23\nLocalidad:Leon");
 }}
